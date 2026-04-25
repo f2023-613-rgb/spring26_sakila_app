d35b6c902b0d4e666fd4ee51b4cbfcd5a3e8882d
Muhammad Nabeel <f2023-613@bnu.edu.pk>
2026-04-25 17:41:25 +0500

    docs: add unit measurement comment for connection timeout 2 more times

--- config.py
+++ config.py
@@ -15,6 +15,8 @@ class Config:
     MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
 
     ## Timeout is measured in seconds
+    ## Timeout is measured in seconds
+    ## Timeout is measured in seconds
     CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
     HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
 
